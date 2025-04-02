79ec15f01d47022151c48c51cbea84b20ca7c2f7
Pritam Kumar <128498859+pritam0852@users.noreply.github.com>
2025-04-02 15:27:50 +0530

    Add files via upload

---
+++ DbConfig.js
@@ -0,0 +1,11 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1.cgdkcu2k6ihf.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : '2rspepsi',
+    DB_DATABASE : 'Webapps'
+});
+
+
+//mysql -h database-1.cgdkcu2k6ihf.us-east-1.rds.amazonaws.com -u admin -p
+//Blue- database-1.cgdkcu2k6ihf.us-east-1.rds.amazonaws.com
+//Green- database-1-green-cge1h6.cgdkcu2k6ihf.us-east-1.rds.amazonaws.com
